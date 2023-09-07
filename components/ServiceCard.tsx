c85968d4cc622e29726359e7606db358ec73b3f8
abdulrehmanjaved-1 <abdulrehmanjaveds12@gmail.com>
2023-09-07 13:41:24 +0500

    done?

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -16,7 +16,7 @@ const ServiceCard: FunctionComponent<{ service: Service }> = ({
       <div className='flex items-center p-2 space-x-4 '>
          <Icon className='w-12 h-12 text-green-500' />
          <div className=''>
-            <h5 className='font-bold'>{title}</h5>
+            <h1 className='font-bold'>{title}</h1>
             <p dangerouslySetInnerHTML={createMarkup()} />
          </div>
       </div>
