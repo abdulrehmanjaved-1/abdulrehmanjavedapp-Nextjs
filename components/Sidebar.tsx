14cc17e21adb004e15b72b6e274b95c93f35b12a
abdulrehmanjaved-1 <abdulrehmanjaveds12@gmail.com>
2023-09-18 09:55:06 +0500

    resume updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,8 +32,8 @@ const Sidebar = () => {
       </p>
       {/* Resume */}
       <a
-        href="/assets/Sumit Dey Resume.pdf"
-        download="Sumit Dey Resume.pdf"
+        href="/assets/myresume.pdf"
+        download="Abdulrehman Javed Resume.pdf"
         className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
       >
         <GiTie className="w-6 h-6" />
